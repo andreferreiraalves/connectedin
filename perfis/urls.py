d34e167b6d35ab59d2c39bda21ea85d69f95b6ff
André Ferreira <andre.ferreira.alves@gmail.com>
2019-01-10 13:55:31 -0200

    criação da tela registrar

--- perfis/urls.py
+++ perfis/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from perfis.views import index, exibir, convidar
+from perfis.views import index, exibir, convidar, aceitar
 
 urlpatterns = [
     path('', index, name='index'),
     path('perfil/<int:perfil_id>', exibir, name='exibir'),
-    path('perfil/<int:perfil_id>/convidar', convidar, name='convidar')
+    path('perfil/<int:perfil_id>/convidar', convidar, name='convidar'),
+    path('perfil/<int:convite_id>/aceitar', aceitar, name='aceitar')
 ]
